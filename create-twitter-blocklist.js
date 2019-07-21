665d18ab781f9dbb06798dd1dd6eda1bb73201e0
Gatovich Programmist <1157916+balamsoft@users.noreply.github.com>
2019-07-21 17:53:09 -0500

    Create create-twitter-blocklist.js

---
+++ create-twitter-blocklist.js
@@ -0,0 +1,30 @@
+var allAccountsCSV = '';
+
+function findBlockableUsers(){
+    return $(".user-actions.not-following");
+}
+
+function findBlockableThenScroll(){
+    console.error("To stop the process simply refresh the page.");
+
+    var blockableUsers = findBlockableUsers();
+    if(!blockableUsers || blockableUsers.length === 0){
+        console.error("-- PROCESS COMPLETE --");
+        console.error(allAccountsCSV);
+        return;
+    }
+    
+    for(var x=0; x < blockableUsers.length; x++){ 
+        allAccountsCSV = allAccountsCSV + '|' + blockableUsers[x].dataset.userId;
+    }
+    
+    $("html, body").animate({ scrollTop: $(document).height() }, 1200);
+
+    setTimeout(function(){
+        console.error("Progress so far: ");
+        console.error(allAccountsCSV);
+        findBlockableThenScroll();
+    }, 2500);
+}
+
+findBlockableThenScroll();
